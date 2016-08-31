2118cc19d4bd2a2775b9ab908766ccb4db8b9b89
tomr <tomr@equalit.ie>
2016-08-31 12:34:36 +0000

    improve dnet separation

--- roles/letsencrypt/files/LE_renew_by_name.sh
+++ roles/letsencrypt/files/LE_renew_by_name.sh
@@ -3,17 +3,20 @@
 # renew only the named certs in LE, when they are about to expire
 # and place all certs where they can be easily synced to brains
 #
-# DOMAINS_DIR should contain one file per certificate, containing
-# all the SNI names the certificate should contain in the format:
-#   maindomain.com -d sub.maindomain.com -d sub.maindomain.com
-#
 #
 
 # define some variables
-LE_DOMAINS_LIST=/opt/autodeflect/etc/letsencrypt-site.list
-LE_OUTPUT_DIR=/opt/autodeflect/le_certs
+BASEDIR=/opt/autodeflect/
+LE_SITE_LIST=${BASEDIR}/etc/letsencrypt-site.list
+LE_OUTPUT_DIR=${BASEDIR}/le_certs
 RENEW_WINDOW=29days   # `man date` for format details
 RENEW_DATE=$(date -d "+$RENEW_WINDOW" +%s)
+LE_CERTS_CONFIG=${BASEDIR}/etc/letsencrypt-certs.auto
+LE_DNETS_CONFIG=${BASEDIR}/etc/letsencrypt-dnets.auto
+
+# split config into relevant sections
+grep '^CERTS' $LE_SITE_LIST | cut -d" " -f 2- > $LE_CERTS_CONFIG
+grep '^DNET' $LE_SITE_LIST | cut -d" " -f 2- > $LE_DNETS_CONFIG
 
 function check_cert() {
   if [ -f /etc/letsencrypt/live/${DOMAIN_W_ARGS%% *}/cert.pem ] ; then
@@ -34,22 +37,17 @@ return $STATUS
 }
 
 function renew_cert() {
-  certbot certonly --standalone --agree-tos --standalone-supported-challenges http-01 -m sysops@equalit.ie -n -d $DOMAIN_W_ARGS
+  echo certbot certonly --standalone --agree-tos --standalone-supported-challenges http-01 -m sysops@equalit.ie -n -d $DOMAIN_W_ARGS
 }
 
 function populate_output_dir() {
-  LE_PREFIX=/etc/letsencrypt/live/${DOMAIN_W_ARGS%% *}
-  cp ${LE_PREFIX}/cert.pem ${LE_OUTPUT_DIR}/${DOMAIN_W_ARGS%% *}.cert.crt
-  cp ${LE_PREFIX}/chain.pem ${LE_OUTPUT_DIR}/${DOMAIN_W_ARGS%% *}.chain.crt
-  cp ${LE_PREFIX}/privkey.pem ${LE_OUTPUT_DIR}/${DOMAIN_W_ARGS%% *}.key
-}
-
-function initialise_output_dir(){
-  [ -d ${LE_OUTPUT_DIR} ] && rm -r ${LE_OUTPUT_DIR}/* || mkdir -p ${LE_OUTPUT_DIR}
+  LE_PREFIX=/etc/letsencrypt/live/${SITEDNET%%:*}
+  cp ${LE_PREFIX}/cert.pem ${LE_OUTPUT_DIR}/${SITEDNET##*:}/${SITEDNET%%:*}.cert.crt
+  cp ${LE_PREFIX}/chain.pem ${LE_OUTPUT_DIR}/${SITEDNET##*:}/${SITEDNET%%:*}.chain.crt
+  cp ${LE_PREFIX}/privkey.pem ${LE_OUTPUT_DIR}/${SITEDNET##*:}/${SITEDNET%%:*}.key
 }
 
 # This is a bit hard to read, so explained in detail:
-# - create an empty directory for cert bundles to end up in
 # - grep out any lines beginning with a '#" character or empty
 # - pipe that output to a while loop that reads line by line
 # - run the "check_cert()" function defined above to see whether
@@ -58,8 +56,12 @@ function initialise_output_dir(){
 #   domains that should be included)
 # - place all the cert/key bundles into an easy dir to be synced
 #   to brains
-initialise_output_dir
-egrep -v '^(#|$)' $LE_DOMAINS_LIST | while IFS='' read -r DOMAIN_W_ARGS ; do
+cat $LE_CERTS_CONFIG | while IFS='' read -r DOMAIN_W_ARGS ; do
   check_cert || renew_cert 
+done
+
+initialise_output_dir
+
+cat $LE_DNETS_CONFIG | while IFS='' read -r SITEDNET ; do
   populate_output_dir
 done
