5a213b7e8f1b821d344e4b34c49d67855a8d4934
RamJett <ramjett@ramnic.com>
2016-09-21 19:19:33 +0000

    initial add of http slip framework

---
+++ http/public/index.php
@@ -0,0 +1,37 @@
+<?php
+require '../vendor/autoload.php';
+
+// Prepare app
+$app = new \Slim\Slim(array(
+    'templates.path' => '../templates',
+));
+
+// Create monolog logger and store logger in container as singleton 
+// (Singleton resources retrieve the same log resource definition each time)
+$app->container->singleton('log', function () {
+    $log = new \Monolog\Logger('slim-skeleton');
+    $log->pushHandler(new \Monolog\Handler\StreamHandler('../logs/app.log', \Monolog\Logger::DEBUG));
+    return $log;
+});
+
+// Prepare view
+$app->view(new \Slim\Views\Twig());
+$app->view->parserOptions = array(
+    'charset' => 'utf-8',
+    'cache' => realpath('../templates/cache'),
+    'auto_reload' => true,
+    'strict_variables' => false,
+    'autoescape' => true
+);
+$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
+
+// Define routes
+$app->get('/', function () use ($app) {
+    // Sample log message
+    $app->log->info("Slim-Skeleton '/' route");
+    // Render index view
+    $app->render('index.html');
+});
+
+// Run app
+$app->run();
