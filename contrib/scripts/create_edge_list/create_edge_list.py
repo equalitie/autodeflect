fbc53e8f8cf2ef3efbb098033d4a2f3ca041ca78
RamJett <ramjett@ramnic.com>
2018-03-08 22:10:25 +0000

    initial commit

---
+++ contrib/scripts/create_edge_list/create_edge_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import glob
+import socket
+
+FAKE_LIST="/usr/local/deflect/etc/whitelist.extra"
+EDGELIST_GLOB="/etc/edgemanage/edges/*"
+
+def main(): 
+    
+    ip_list = []
+    with open(FAKE_LIST) as fake_f: 
+        ip_list += fake_f.read().strip().split("\n")
+
+    for edgelist_path in glob.glob(EDGELIST_GLOB): 
+        if os.path.isfile(edgelist_path): 
+            with open(edgelist_path) as edgelist_f: 
+                for edgename in edgelist_f.read().strip().split("\n"): 
+                    if edgename.startswith("#"):
+                        # skip commented edges
+                        continue
+                    edge_ip = socket.gethostbyname(edgename)
+                    if edge_ip not in ip_list: 
+                        ip_list.append(edge_ip)
+
+    print "\n".join(sorted(ip_list))
+
+if __name__ == "__main__":
+    main()
